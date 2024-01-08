eef7c2a50e0c8d884d5486a0503ca02bc72d8a42
prattiikk <pvks5423@gmail.com>
2024-01-09 01:31:47 +0530

    fixed transaction section , replaced flex with grid system

--- src/Components/OrderCard.jsx
+++ src/Components/OrderCard.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const OrderCard = ({ id, date, amt, fees }) => {
   return (
-    <div className="w-full h-[50px] flex items-center justify-between text-sm font-medium bg-white border-b px-4">
-      <span className=" text-sky-700">#{id}</span>
-      <span>{date}</span>
-      <span>{amt}</span>
-      <span>{fees}</span>
+    <div className="w-full h-[50px] flex items-center  text-sm font-medium bg-white border-b px-4">
+      <div className="w-full grid grid-cols-4 grid-rows-1">
+        <span className=" text-sky-700 col-span-1">#{id}</span>
+        <span className=" col-span-1">{date}</span>
+        <span className=" col-span-1 flex justify-center">{amt}</span>
+        <span className=" col-span-1 flex justify-end">{fees}</span>
+      </div>
     </div>
   );
 };
--- src/Components/Transactions.jsx
+++ src/Components/Transactions.jsx
@@ -5,27 +5,33 @@ import { LuSearch } from "react-icons/lu";
 const Transactions = () => {
   return (
     <div className="w-full mt-0">
-      <div className="text-xl font-medium h-[60px]">
+      <div className="text-xl font-medium h-[40px]">
         Transactions | This Month
       </div>
       <div className="w-full p-4 bg-white shadow-sm">
-        <div className=" w-[260px] flex items-center relative">
+        <div className=" w-[260px] flex items-center relative my-2">
           <div className="absolute h-[60%] p-4 pl-4 left-0 flex items-center scale-125 pointer-events-none opacity-40 ">
             <LuSearch />
           </div>
           <input
             type="search"
             name="search"
-            placeholder="Search features, tutorials, etc."
+            placeholder="Search by order ID..."
             id="search"
             className="w-full h-[40px] p-4 pl-10 rounded-lg bg-white border"
           />
         </div>
         <div className="w-full h-[40px] rounded-md flex items-center justify-between text-sm font-medium bg-stone-200 border-b px-4">
-          <span>Order ID</span>
-          <span>Order date</span>
-          <span>Order amount</span>
-          <span>Transaction fees</span>
+          <div className="w-full grid grid-cols-4 grid-rows-1">
+            <span className=" col-span-1">Order ID</span>
+            <span className=" col-span-1 ">Order date</span>
+            <span className=" col-span-1 flex justify-center">
+              Order amount
+            </span>
+            <span className=" col-span-1 flex justify-end">
+              Transaction fees
+            </span>
+          </div>
         </div>
         <OrderCard
           id={281209}
